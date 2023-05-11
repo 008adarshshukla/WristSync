5b8ee6e7077e5cc108a1f38489a531b08ae9b596
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-05-11 21:25:14 +0530

    first commit

---
+++ WristSync_Watch Watch App/WristSync_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristSync_WatchApp.swift
+//  WristSync_Watch Watch App
+//
+//  Created by Adarsh Shukla on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WristSync_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
